a36c14e4d999fa2d198c3b637239fc1b3fc35cc9
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-23 22:32:30 +0700

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
+}
+
+android {
+    namespace = "jet.notes"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "jet.notes"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        androidResources {
+            localeFilters += listOf("en")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.compose.ui:ui:1.9.0-beta02")
+    implementation("androidx.compose.material3:material3:1.4.0-alpha18")
+    implementation("androidx.activity:activity-compose:1.10.1")
+}
