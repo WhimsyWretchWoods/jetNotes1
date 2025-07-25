197b0478d5a4f3ca60b31012008c99dfa9263dcf
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-25 20:46:39 +0700

    Create Theme.kt

---
+++ app/src/main/java/jet/notes/ui/theme/Theme.kt
@@ -0,0 +1,17 @@
+package jet.notes.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColorScheme()
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    val colors = DarkColorScheme
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
