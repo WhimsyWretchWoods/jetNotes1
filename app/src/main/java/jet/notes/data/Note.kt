fd7d9b2a9d2db80d37df2895e4639f7e1103e797
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-25 20:42:31 +0700

    Create Note.kt

---
+++ app/src/main/java/jet/notes/data/Note.kt
@@ -0,0 +1,14 @@
+package jet.notes.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "notes_table")
+data class Note(
+    @PrimaryKey
+    val id: String = UUID.randomUUID().toString(),
+    var title: String,
+    var content: String,
+    val timestamp: Long = System.currentTimeMillis()
+)
