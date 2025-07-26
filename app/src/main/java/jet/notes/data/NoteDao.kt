cfc227e1517ceb382f8efe2af91df15a40293165
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-26 17:56:14 +0700

    Update NoteDao.kt

--- app/src/main/java/jet/notes/data/NoteDao.kt
+++ app/src/main/java/jet/notes/data/NoteDao.kt
@@ -1,6 +1,7 @@
 package jet.notes.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -9,6 +10,11 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
+    @Query("SELECT * FROM notes ORDER BY timestamp DESC")
+    fun getAllNotes(): Flow<List<Note>>
+
+    @Query("SELECT * FROM notes WHERE id = :noteId")
+    suspend fun getNoteById(noteId: String): Note?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(note: Note)
@@ -16,15 +22,9 @@ interface NoteDao {
     @Update
     suspend fun update(note: Note)
 
-    @Query("SELECT * FROM notes_table WHERE id = :noteId")
-    fun getNoteById(noteId: String): Flow<Note?>
+    @Delete
+    suspend fun delete(note: Note)
 
-    @Query("SELECT * FROM notes_table ORDER BY timestamp DESC")
-    fun getAllNotes(): Flow<List<Note>>
-
-    @Query("DELETE FROM notes_table WHERE id = :noteId")
-    suspend fun deleteNoteById(noteId: String)
-
-    @Query("DELETE FROM notes_table")
-    suspend fun deleteAllNotes()
+    @Query("DELETE FROM notes")
+    suspend fun deleteAll()
 }
