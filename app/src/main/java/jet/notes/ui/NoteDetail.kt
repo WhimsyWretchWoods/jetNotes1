b0b1881b6dee0947cadaea5edee34d23a303de12
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-26 18:00:26 +0700

    Update NoteDetail.kt

--- app/src/main/java/jet/notes/ui/NoteDetail.kt
+++ app/src/main/java/jet/notes/ui/NoteDetail.kt
@@ -1,38 +1,5 @@
 package jet.notes.ui
 
-import androidx.compose.runtime.Composable
-import androidx.compose.material3.Text
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.*
-import androidx.navigation.NavController
-import androidx.compose.runtime.LaunchedEffect
-
-import androidx.lifecycle.viewmodel.compose.viewModel
-
-import jet.notes.viewmodel.NoteViewModel
-import jet.notes.data.Note
-
-@OptIn(ExperimentalMaterial3Api::class)
-package jet.notes.ui
-
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
