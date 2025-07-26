9ec9bd2f094f74ad1f3b7480b41d56b968bea4f6
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-26 17:54:36 +0700

    Update MainActivity.kt

--- app/src/main/java/jet/notes/MainActivity.kt
+++ app/src/main/java/jet/notes/MainActivity.kt
@@ -1,34 +1,32 @@
 package jet.notes
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.ComponentActivity
-
-import androidx.compose.ui.Modifier
-import androidx.compose.material3.Surface
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.foundation.layout.fillMaxSize
-
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-
+import jet.notes.data.NoteDatabase
 import jet.notes.navigation.NoteApp
 import jet.notes.ui.theme.AppTheme
 import jet.notes.viewmodel.NoteViewModel
 
 class MainActivity : ComponentActivity() {
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		enableEdgeToEdge()
-		setContent {
-		    AppTheme {
-		        val application = application as NoteApplication
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                val noteDao = NoteDatabase.getDatabase(applicationContext).noteDao()
                 val noteViewModel: NoteViewModel = viewModel(
-                    factory = NoteViewModel.provideFactory(application.repository))
-		        Surface(modifier = Modifier.fillMaxSize()) {
-		            NoteApp(noteViewModel = noteViewModel)
-		        }
-		    }
-		}
+                    factory = NoteViewModel.provideFactory(noteDao)
+                )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    NoteApp(noteViewModel = noteViewModel)
+                }
+            }
+        }
     }
 }
